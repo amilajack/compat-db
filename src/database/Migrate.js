a1fc8d00a5c0d4bdb19f372135e5a5d64987d92b
Amila Welihinda <amilajack@gmail.com>
2017-02-24 21:27:31 -0800

    Fixed record metadata migration

--- src/database/Migrate.js
+++ src/database/Migrate.js
@@ -1,12 +1,15 @@
-import { migrate } from './TmpDatabase';
+import * as TmpDatabase from './TmpDatabase';
 import JobQueueDatabase from '../../src/database/JobQueueDatabase';
+import RecordMetadataDatabase from '../../src/database/RecordMetadataDatabase';
 
 
-migrate();
+TmpDatabase.migrate();
 
 const jobQueue = new JobQueueDatabase();
-
 jobQueue
   .migrate()
   .then(() => process.exit(0))
   .catch(console.log);
+
+const recordMetadata = new RecordMetadataDatabase();
+recordMetadata.migrate();
